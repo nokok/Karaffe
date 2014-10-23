7d86c6a056b8f5e7e2bd487b37fa5e8e31118f37
nokok <nokok.kz@gmail.com>
2014-10-23 20:29:52 +0900

    nullチェック追加

--- src/main/java/net/nokok/karaffe/javacc/Name.java
+++ src/main/java/net/nokok/karaffe/javacc/Name.java
@@ -1,11 +1,13 @@
 package net.nokok.karaffe.javacc;
 
+import static java.util.Objects.requireNonNull;
+
 public class Name {
 
     private final String name;
 
     public Name(String name) {
-        this.name = name;
+        this.name = requireNonNull(name);
     }
 
     @Override
