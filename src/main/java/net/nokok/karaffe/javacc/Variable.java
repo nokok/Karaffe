d1e75ef89acaa44778b981c3c0c3030469f4dcb7
nokok <nokok.kz@gmail.com>
2014-10-23 20:40:14 +0900

    Variableクラスにコンストラクタと関連するフィールドを追加

--- src/main/java/net/nokok/karaffe/javacc/Variable.java
+++ src/main/java/net/nokok/karaffe/javacc/Variable.java
@@ -2,4 +2,11 @@ package net.nokok.karaffe.javacc;
 
 public class Variable {
 
+    private final Name name;
+    private final TypeDeclaration type;
+
+    public Variable(Name name, TypeDeclaration type) {
+        this.name = name;
+        this.type = type;
+    }
 }
