f0fd7b537041ef2bf3196cd88f09f04b6d3a3ba3
nokok <nokok.kz@gmail.com>
2014-10-23 23:27:55 +0900

    javadoc追加

--- src/main/java/net/nokok/karaffe/javacc/stmt/Statement.java
+++ src/main/java/net/nokok/karaffe/javacc/stmt/Statement.java
@@ -2,5 +2,13 @@ package net.nokok.karaffe.javacc.stmt;
 
 public interface Statement {
 
+    /**
+     * @return ステートメントの種類
+     */
     public StatementType getType();
+
+    /**
+     * @return seffect演算子を許可する場合true
+     */
+    public boolean isAllowingSideEffectModifier();
 }
