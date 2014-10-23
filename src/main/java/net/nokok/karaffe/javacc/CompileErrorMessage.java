60dff4f2b27b3b936cb08eb26b2143cc29e0c78c
nokok <nokok.kz@gmail.com>
2014-10-23 23:26:27 +0900

    エラーメッセージ追加

--- src/main/java/net/nokok/karaffe/javacc/CompileErrorMessage.java
+++ src/main/java/net/nokok/karaffe/javacc/CompileErrorMessage.java
@@ -2,8 +2,9 @@ package net.nokok.karaffe.javacc;
 
 public enum CompileErrorMessage {
 
-    TYPENAME_MUST_BE_START_WITH_UPPER_CASE("型名は大文字から始める必要があります"),;
     TYPE_NOT_FOUND("型が見つかりません"),
+    TYPENAME_MUST_BE_START_WITH_UPPER_CASE("型名は大文字から始める必要があります"),
+    TYPE_ALIAS_TYPE_PARAMETER("Type Aliasで型パラメータは使用できません"),;
 
     private final String message;
 
