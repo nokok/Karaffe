8f04ee71a0e6819068d90a4eb3a6acdfbdbc7574
nokok <nokok.kz@gmail.com>
2014-10-26 21:56:44 +0900

    TypeTestを修正

--- src/test/java/net/nokok/karaffe/javacc/TypeIdTest.java
+++ src/test/java/net/nokok/karaffe/javacc/TypeIdTest.java
@@ -1,33 +1,34 @@
 package net.nokok.karaffe.javacc;
 
+import net.nokok.karaffe.javacc.identifier.TypeId;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
-public class TypeTest {
+public class TypeIdTest {
 
     @Test(expected = RuntimeException.class)
     public void testStartWithLowerCaseTypeName() {
-        Type t = new Type(new Name("hoge"));
+        TypeId t = new TypeId(new Name("hoge"));
     }
 
     @Test
     public void testIsTypeParameter() {
-        Type t = new Type(new Name("a"));
+        TypeId t = new TypeId(new Name("a"));
         assertThat(t.isTypeParameter(), is(true));
         assertThat(t.isInterface(), is(false));
     }
 
     @Test
     public void testIsNotTypeParameter() {
-        Type t = new Type(new Name("HogeType"));
+        TypeId t = new TypeId(new Name("HogeType"));
         assertThat(t.isTypeParameter(), is(false));
         assertThat(t.isInterface(), is(false));
     }
 
     @Test
     public void testIsInterface() {
-        Type t = new Type(new Name("IHoge"));
+        TypeId t = new TypeId(new Name("IHoge"));
         assertThat(t.isInterface(), is(true));
     }
 
