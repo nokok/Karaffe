4affee1a622c8846037db242682ea1509e46960e
nokok <nokok.kz@gmail.com>
2014-10-28 01:17:08 +0900

    StatementTypeを追加

--- src/main/java/net/nokok/karaffe/javacc/stmt/StatementType.java
+++ src/main/java/net/nokok/karaffe/javacc/stmt/StatementType.java
@@ -22,4 +22,16 @@ public enum StatementType {
      * 関数宣言
      */
     FUNCTION_DECLARATION,
+    /**
+     * seffect修飾子付き関数宣言
+     */
+    S_FUNCTION_DECLARATION,
+    /**
+     * 変数宣言
+     */
+    VARIABLE_DECLARATION,
+    /**
+     * seffect修飾子付き変数宣言
+     */
+    S_VARIABLE_DECLARATION,
 }
