df53746505b1083cc2278fd751e8feeaaa78591b
nokok <nokok.kz@gmail.com>
2014-10-27 22:56:02 +0900

    MutableVariableDeclarationクラスの追加

---
+++ src/main/java/net/nokok/karaffe/javacc/stmt/MutableVariableDeclaration.java
@@ -0,0 +1,25 @@
+package net.nokok.karaffe.javacc.stmt;
+
+import net.nokok.karaffe.javacc.expr.*;
+import net.nokok.karaffe.javacc.identifier.TypeId;
+import net.nokok.karaffe.javacc.identifier.VariableId;
+
+public class MutableVariableDeclaration implements Statement {
+
+    private final VariableDeclaration variableDeclaration;
+
+    public MutableVariableDeclaration(VariableId name, TypeId type, Expression<?, ?> expr) {
+        variableDeclaration = new VariableDeclaration(name, type, expr);
+    }
+
+    @Override
+    public StatementType getType() {
+        return StatementType.S_VARIABLE_DECLARATION;
+    }
+
+    @Override
+    public boolean isAllowingSideEffectModifier() {
+        return true;
+    }
+
+}
