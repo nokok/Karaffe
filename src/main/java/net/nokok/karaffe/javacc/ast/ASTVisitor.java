c953d15fe9d0ee7fea1e1b5c645d62ef6af6b4a0
nokok <nokok.kz@gmail.com>
2014-10-30 23:12:45 +0900

    ASTVisitorにメソッドを追加

--- src/main/java/net/nokok/karaffe/javacc/ast/ASTVisitor.java
+++ src/main/java/net/nokok/karaffe/javacc/ast/ASTVisitor.java
@@ -2,8 +2,34 @@ package net.nokok.karaffe.javacc.ast;
 
 public interface ASTVisitor {
 
+    public Object onBoolLiteral(BoolLiteral aThis);
+
+    public Object onFixedSizeArrayLiteral(FixedSizeArrayLiteral aThis);
+
+    public Object onFloatLiteral(FloatLiteral aThis);
+
+    public Object onFunctionLiteral(FunctionLiteral aThis);
+
+    public Object onIntLiteral(IntLiteral aThis);
+
+    public Object onJavaTypeLiteral(JavaTypeLiteral aThis);
+
+    public Object onKaraffeTypeLiteral(KaraffeTypeLiteral aThis);
+
     public Object onProgram(Program aThis);
 
     public Object onPrograms(Programs aThis);
+
+    public Object onStringLiteral(StringLiteral aThis);
+
+    public Object onTypeId(TypeId aThis);
+
+    public Object onTypeParameterId(TypeParameterId aThis);
+
+    public Object onTypeParameters(TypeParameters aThis);
+
+    public Object onUndefinedLiteral(UndefinedLiteral aThis);
+
+    public Object onVaraibleId(VariableId aThis);
 
 }
