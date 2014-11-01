077acea4d0062c8348d7ab5e7ce235d20ea1d3b3
nokok <nokok.kz@gmail.com>
2014-11-01 13:16:24 +0900

    Optional化

--- src/main/java/net/nokok/karaffe/javacc/ast/FunctionLiteral.java
+++ src/main/java/net/nokok/karaffe/javacc/ast/FunctionLiteral.java
@@ -1,9 +1,11 @@
 package net.nokok.karaffe.javacc.ast;
 
+import java.util.Optional;
+
 public class FunctionLiteral extends Literal<Expression<?, ?>> {
 
-    private final VariableIdTypePairs varIdPairs;
-    private final VariableList varList;
+    private final Optional<VariableIdTypePairs> varIdPairs;
+    private final Optional<VariableList> varList;
 
     public FunctionLiteral(Expression<?, ?> value) {
         super(value);
@@ -13,14 +15,14 @@ public class FunctionLiteral extends Literal<Expression<?, ?>> {
 
     FunctionLiteral(VariableIdTypePairs varIdPairs, Expression<?, ?> expr) {
         super(expr);
-        this.varIdPairs = varIdPairs;
-        this.varList = null;
+        this.varIdPairs = Optional.of(varIdPairs);
+        this.varList = Optional.empty();
     }
 
     FunctionLiteral(VariableList varList, Expression<?, ?> expr) {
         super(expr);
-        this.varList = varList;
-        this.varIdPairs = null;
+        this.varList = Optional.of(varList);
+        this.varIdPairs = Optional.empty();
     }
 
     @Override
