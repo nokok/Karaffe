be8b1d0f2525dfb98882c98a63dd53c1459b8acc
nokok <nokok.kz@gmail.com>
2014-11-04 22:52:44 +0900

    Programクラスにメソッドを追加
    addElement
    getNodes

--- src/main/java/net/nokok/karaffe/javacc/ast/Program.java
+++ src/main/java/net/nokok/karaffe/javacc/ast/Program.java
@@ -1,10 +1,23 @@
 package net.nokok.karaffe.javacc.ast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program implements ASTNode {
+
+    private final List<ASTNode> nodes = new ArrayList<>();
 
     @Override
     public Object accept(ASTVisitor visitor) {
         return visitor.onProgram(this);
+    }
+
+    public void addElement(ASTNode node) {
+        nodes.add(node);
+    }
+
+    public List<ASTNode> getNodes() {
+        return new ArrayList<>(nodes);
     }
 
     @Override
