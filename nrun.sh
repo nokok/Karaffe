ccc82059e98faf18ca59a3f60396603ae15ffd4c
nokok <nokok.kz@gmail.com>
2014-12-10 02:09:14 +0900

    スクリプトの更新
    jjdocの実行
    jarのパスを変数へ

--- nrun.sh
+++ nrun.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd ./src/main/java/net/nokok/karaffe/parser
-java -cp ../../../../../../../lib/javacc.jar jjtree karaffe.jjt
-java -cp ../../../../../../../lib/javacc.jar javacc karaffe.jj
 rm AST*.java TokenMgrError.java ParseException.java Token.java SimpleCharStream.java karaffe.jj
+jarPath='../../../../../../../lib/javacc.jar'
+java -cp $jarPath jjtree karaffe.jjt
+java -cp $jarPath javacc karaffe.jj
+java -cp $jarPath jjdoc karaffe.jj
