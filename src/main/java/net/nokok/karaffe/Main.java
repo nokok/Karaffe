791c17752f1a406deedda2d8b72ec56737cc07c2
nokok <nokok.kz@gmail.com>
2014-12-13 23:02:47 +0900

    wip

--- src/main/java/net/nokok/karaffe/Main.java
+++ src/main/java/net/nokok/karaffe/Main.java
@@ -2,8 +2,14 @@ package net.nokok.karaffe;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import net.nokok.karaffe.parser.ASTCompileUnit;
 import net.nokok.karaffe.parser.KaraffeParser;
+import net.nokok.karaffe.parser.ParseException;
+import net.nokok.karaffe.parser.excptn.KaraffeParserException;
+import net.nokok.karaffe.parser.util.OutputFormatter;
+import net.nokok.karaffe.parser.visitor.BytecodeGenerator;
+import net.nokok.karaffe.parser.visitor.PrintAST;
 
 public class Main {
 
@@ -12,30 +18,20 @@ public class Main {
      *
      * @param args
      */
-    public static void main(String... args) throws Exception {
+    public static void main(String... args) throws IOException, KaraffeParserException {
         args = new String[]{"Int.krf"};
         for (String arg : args) {
-            File file = new File(arg);
-            KaraffeParser parser = new KaraffeParser(new FileReader(file));
-            ASTCompileUnit compileUnit = parser.CompileUnit();
-//            BytecodeGenerator bytecodeGenerator = new BytecodeGenerator(file.getName());
-//            compileUnit.jjtAccept(bytecodeGenerator, null);
-//            Map<String, byte[]> classes = bytecodeGenerator.toByteArrays();
-//            classes.entrySet().stream().forEach(clazz -> {
-//                try {
-//                    String className = clazz.getKey();
-//                    byte[] bytecode = clazz.getValue();
-//                    //クラスパス上にバイトコードを出力する
-//                    Path path = new File(new File(Main.class.getClassLoader().getResource("").getPath()), className + ".class").toPath();
-//                    if (Files.exists(path)) {
-//                        Files.delete(path);
-//                    }
-//                    Files.write(path, bytecode, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);
-//                } catch (IOException ex) {
-//                    ex.printStackTrace();
-//                }
-//            });
-            compileUnit.dump("");
+            try {
+                File file = new File(arg);
+                KaraffeParser parser = new KaraffeParser(new FileReader(file));
+                ASTCompileUnit compileUnit = parser.CompileUnit();
+                compileUnit.dump("");
+                compileUnit.jjtAccept(new PrintAST(), null);
+                compileUnit.jjtAccept(new BytecodeGenerator(arg), null);
+            } catch (ParseException ex) {
+                ex.printStackTrace();
+                new OutputFormatter(arg).print(ex);
+            }
         }
     }
 }
