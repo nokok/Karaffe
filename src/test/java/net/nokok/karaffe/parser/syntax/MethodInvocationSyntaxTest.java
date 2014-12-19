cbe2b13ed9616473ac4f98020b6823e3a08483f0
nokok <nokok.kz@gmail.com>
2014-12-19 23:51:02 +0900

    不足しているファイルを追加

---
+++ src/test/java/net/nokok/karaffe/parser/syntax/MethodInvocationSyntaxTest.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * Karaffe Programming Language
+ *   __ _____   ___  ___   ____________
+ *   / //_/ _ | / _ \/ _ | / __/ __/ __/
+ *  / , \/ __ |/ , _/ __ |/ _// _// _/
+ * /_/|_/_/ |_/_/|_/_/ |_/_/ /_/ /___/
+ *
+ */
+package net.nokok.karaffe.parser.syntax;
+
+import static net.nokok.karaffe.parser.syntax.KaraffeParserSyntaxTest.testCode;
+import org.junit.Test;
+
+public class MethodInvocationSyntaxTest {
+
+    @Test
+    public void testMethodNameArgumentList() {
+        testCode("println(\"hoge\")");
+    }
+}
