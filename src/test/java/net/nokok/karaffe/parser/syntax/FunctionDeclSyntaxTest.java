24c4c9943c3eb70e1320409a2e06ed7450bb6da1
nokok <nokok.kz@gmail.com>
2014-12-21 18:52:40 +0900

    endmodule文を削除

---
+++ src/test/java/net/nokok/karaffe/parser/syntax/FunctionDeclSyntaxTest.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * Karaffe Programming Language
+ *   __ _____   ___  ___   ____________
+ *   / //_/ _ | / _ \/ _ | / __/ __/ __/
+ *  / , \/ __ |/ , _/ __ |/ _// _// _/
+ * /_/|_/_/ |_/_/|_/_/ |_/_/ /_/ /___/
+ *
+ */
+package net.nokok.karaffe.parser.syntax;
+
+import static net.nokok.karaffe.parser.syntax.KaraffeParserSyntaxTest.testCode;
+import org.junit.Test;
+
+public class FunctionDeclSyntaxTest {
+
+    @Test
+    public void testSimpleFunctionDeclSyntax() {
+        testCode("func : Int -> Int = undefined");
+    }
+
+    @Test
+    public void testSimpleCurriedFunctionDeclSyntax() {
+        testCode("func : Int -> Int -> Int = undefined");
+    }
+
+    @Test
+    public void testUncurriedFunctionDeclSyntax() {
+        testCode("func : Int Int -> Int = undefined");
+    }
+
+    @Test
+    public void testMixedFunctionDeclSyntax() {
+        testCode("func : Int -> Int String -> Int -> Int = undefined");
+    }
+}
---
+++ src/test/java/net/nokok/karaffe/parser/syntax/FunctionLiteralSyntaxTest.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * Karaffe Programming Language
+ *   __ _____   ___  ___   ____________
+ *   / //_/ _ | / _ \/ _ | / __/ __/ __/
+ *  / , \/ __ |/ , _/ __ |/ _// _// _/
+ * /_/|_/_/ |_/_/|_/_/ |_/_/ /_/ /___/
+ *
+ */
+package net.nokok.karaffe.parser.syntax;
+
+import static net.nokok.karaffe.parser.syntax.KaraffeParserSyntaxTest.testCode;
+import org.junit.Test;
+
+public class FunctionLiteralSyntaxTest {
+
+    @Test
+    public void testSimpleFunctionLiteral() {
+        testCode("println ((_) -> _ + 1) 2\n");
+    }
+}
