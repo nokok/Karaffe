965e77f4a9a02fc988bbc69c76c6acc4274b8c77
nokok <nokok.kz@gmail.com>
2014-12-27 16:21:59 +0900

    Visitorを追加

---
+++ src/main/java/net/nokok/karaffe/parser/visitor/FuncDeclVisitor.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * Karaffe Programming Language
+ *   __ _____   ___  ___   ____________
+ *   / //_/ _ | / _ \/ _ | / __/ __/ __/
+ *  / , \/ __ |/ , _/ __ |/ _// _// _/
+ * /_/|_/_/ |_/_/|_/_/ |_/_/ /_/ /___/
+ *
+ */
+package net.nokok.karaffe.parser.visitor;
+
+import net.nokok.karaffe.parser.KaraffeParserDefaultVisitor;
+import net.nokok.karaffe.parser.util.CurrentState;
+
+public class FuncDeclVisitor extends KaraffeParserDefaultVisitor {
+
+    public FuncDeclVisitor(CurrentState state) {
+    }
+}
---
+++ src/main/java/net/nokok/karaffe/parser/visitor/VarDeclVisitor.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * Karaffe Programming Language
+ * __ _____ ___ ___ ____________
+ *   / //_/ _ | / _ \/ _ | / __/ __/ __/
+ *  / , \/ __ |/ , _/ __ |/ _// _// _/
+ * /_/|_/_/ |_/_/|_/_/ |_/_/ /_/ /___/
+ *
+ */
+package net.nokok.karaffe.parser.visitor;
+
+import net.nokok.karaffe.parser.KaraffeParserDefaultVisitor;
+import net.nokok.karaffe.parser.util.CurrentState;
+
+public class VarDeclVisitor extends KaraffeParserDefaultVisitor {
+
+    public VarDeclVisitor(CurrentState state) {
+
+    }
+}
