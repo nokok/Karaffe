70546e5d5aa4e964398c991064cf29a4cc49cd87
noko <nokok.kz@gmail.com>
2015-02-21 15:49:04 +0900

    Gen.javaを削除

--- src/main/java/net/nokok/karaffe/Main.java
+++ src/main/java/net/nokok/karaffe/Main.java
@@ -3,11 +3,17 @@ package net.nokok.karaffe;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import net.nokok.karaffe.parser.ASTCompileUnit;
 import net.nokok.karaffe.parser.ParseException;
 import net.nokok.karaffe.parser.Parser;
-import net.nokok.karaffe.parser.asm.Gen;
+import net.nokok.karaffe.parser.asm.nodes.collectors.ClassCollector;
+import net.nokok.karaffe.parser.asm.nodes.collectors.ImportCollector;
+import net.nokok.karaffe.parser.asm.typechecker.ClassResolver;
 import net.nokok.karaffe.parser.util.OutputFormatter;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
 
 public class Main {
 
@@ -25,7 +31,15 @@ public class Main {
                 final Parser parser = new Parser(new FileReader(file));
                 final ASTCompileUnit compileUnit = parser.CompileUnit();
                 compileUnit.dump(""); //Debug
-                final Gen gen = new Gen(compileUnit);
+                Map<String, Class<?>> imports = new ImportCollector(compileUnit).getImports();
+                ClassResolver resolver = new ClassResolver(imports);
+                ClassCollector classCollector = new ClassCollector(resolver);
+                List<ClassNode> classes = classCollector.getClassNodes();
+                classes.forEach(clazz -> {
+                    ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+                    clazz.accept(classWriter);
+                    classWriter.toByteArray();
+                });
             } catch (ParseException ex) {
                 new OutputFormatter(arg).print(ex);
             } catch (IOException ex) {
