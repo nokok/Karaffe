739764f9f35f6c49250f78bb0465954a4ba8989b
noko <nokok.kz@gmail.com>
2015-04-18 23:01:41 +0900

    Type周りを修正

--- src/main/java/karaffe/compiler/tree/type/Type.java
+++ src/main/java/karaffe/compiler/tree/type/Type.java
@@ -3,22 +3,28 @@
  */
 package karaffe.compiler.tree.type;
 
+import java.util.Optional;
 import karaffe.compiler.tree.AST;
 import karaffe.compiler.tree.AbstractNode;
 import karaffe.compiler.visitor.Visitor;
 
 public class Type extends AbstractNode {
 
-    private final AST type;
+    private final Optional<AST> type;
 
     public Type(Object t) {
-        this.type = (AST) t;
+        this.type = Optional.ofNullable((AST) t);
         addChildren(type);
     }
 
     @Override
     public void accept(Visitor visitor) {
         visitor.type(this);
+    }
+
+    @Override
+    public String toString() {
+        return "(Type:" + type.toString() + ")";
     }
 
 }
