de4b657ab386651c1752e03fd497900dfbb24c6b
noko <nokok.kz@gmail.com>
2015-04-20 00:24:51 +0900

    ClassGenの修正

--- src/main/java/karaffe/compiler/phase/gencode/ClassGen.java
+++ src/main/java/karaffe/compiler/phase/gencode/ClassGen.java
@@ -10,12 +10,14 @@ import java.util.function.Function;
 import karaffe.compiler.tree.AST;
 import karaffe.compiler.tree.classdecls.FieldDecl;
 import karaffe.compiler.tree.classdecls.SimpleClassDecl;
+import karaffe.compiler.tree.compileunits.CompileUnit;
 import karaffe.compiler.tree.compileunits.FileNode;
 import karaffe.compiler.tree.compileunits.PackageDecl;
 import karaffe.compiler.visitor.Visitor;
 import karaffe.compiler.visitor.VisitorAdaptor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
 
 public class ClassGen implements Function<AST, List<ByteCode>> {
 
@@ -26,6 +28,8 @@ public class ClassGen implements Function<AST, List<ByteCode>> {
 
         private Optional<String> packagePrefix = Optional.empty();
 
+        private final List<ClassNode> classNodes = new ArrayList<>();
+
         @Override
         public void fileNode(FileNode aThis) {
             super.fileNode(aThis);
@@ -35,6 +39,7 @@ public class ClassGen implements Function<AST, List<ByteCode>> {
         @Override
         public void simpleClassDecl(SimpleClassDecl clazz) {
             super.simpleClassDecl(clazz);
+            classNodes.add(clazz.toNode());
             classWriter.visit(Opcodes.V1_8, clazz.access(), getClassNamePrefix() + clazz.name(), clazz.signature(), clazz.superName(), clazz.interfaces());
             bytecodes.add(new ByteCode(classWriter.toByteArray(), clazz.name() + ".class", packagePrefix.orElse("")));
         }
@@ -61,8 +66,8 @@ public class ClassGen implements Function<AST, List<ByteCode>> {
 
     @Override
     public List<ByteCode> apply(AST ast) {
-        ast.accept(classVisitor);
-        return bytecodes;
+        CompileUnit compileUnit = (CompileUnit) ast;
+        return compileUnit.toNode();
     }
 
 }
