3e3c9ff7d24cc416069900487a937829738b41b6
noko <nokok.kz@gmail.com>
2015-04-25 16:49:05 +0900

    wip

--- src/main/java/karaffe/compiler/tree/modifiers/ModifierList.java
+++ src/main/java/karaffe/compiler/tree/modifiers/ModifierList.java
@@ -4,11 +4,13 @@
 package karaffe.compiler.tree.modifiers;
 
 import java.util.Optional;
+import karaffe.compiler.tree.ASMConvertible;
 import karaffe.compiler.tree.AST;
 import karaffe.compiler.tree.AbstractNode;
+import karaffe.compiler.tree.util.ModifierData;
 import karaffe.compiler.visitor.Visitor;
 
-public class ModifierList extends AbstractNode {
+public class ModifierList extends AbstractNode implements ASMConvertible<ModifierData> {
 
     private final AST modifier;
     private final Optional<AST> modifierList;
@@ -21,6 +23,21 @@ public class ModifierList extends AbstractNode {
     @Override
     public void accept(Visitor visitor) {
         visitor.modifierList(this);
+    }
+
+    @Override
+    public ModifierData toNode() {
+        return new ModifierData();
+    }
+
+    public AbstractModifier first() {
+        return AbstractModifier.class.cast(modifier);
+    }
+
+    public Optional<AbstractModifier> nextModifier() {
+        return modifierList
+                .map(ModifierList.class::cast)
+                .map(ModifierList::first);
     }
 
 }
