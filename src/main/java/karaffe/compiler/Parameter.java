a872d5f4d2a6aa0294eb276afe05fe3843e91d2c
noko <nokok.kz@gmail.com>
2016-02-14 23:26:27 +0900

    Parameterクラスのメソッドを追加

--- src/main/java/karaffe/compiler/Parameter.java
+++ src/main/java/karaffe/compiler/Parameter.java
@@ -1,15 +1,18 @@
 package karaffe.compiler;
 
 import java.util.Objects;
+
 class Parameter {
 
     private final Identifier id;
     private final TypeElement type;
+    private MethodDef parent;
     private String path;
 
     Parameter(Identifier id, TypeElement type) {
         this.id = id;
         this.type = type;
+        Context.INSTANCE.add(this);
     }
 
     public String id() {
@@ -25,8 +28,27 @@ class Parameter {
         return "(parameter " + id + " " + type + ")";
     }
 
+    public void setParent(MethodDef methodDef) {
+        if ( this.parent != null ) {
+
+        }
+        this.parent = Objects.requireNonNull(methodDef);
+    }
+
+    public MethodDef getParent() {
+        return parent;
+    }
+
     public String getPath() {
         return path;
+    }
+
+    public TypeElement getTypeElement() {
+        return type;
+    }
+
+    public Expression getExpr() {
+        return Expression.UNINITIALIZED;
     }
 
     public void setPath(String path) {
@@ -34,5 +56,9 @@ class Parameter {
             throw new IllegalStateException(id());
         }
         this.path = Objects.requireNonNull(path);
+    }
+
+    public Position position() {
+        return new Position(id.getLine(), id.getColumn());
     }
 }
