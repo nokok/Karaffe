94e4a2c41dad5bc51eb087bda8de21a8b78ef53e
noko <nokok.kz@gmail.com>
2016-02-14 23:16:32 +0900

    Expressionに定数UNINITIALIZEDを追加

--- src/main/java/karaffe/compiler/Expression.java
+++ src/main/java/karaffe/compiler/Expression.java
@@ -1,7 +1,23 @@
 package karaffe.compiler;
 
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
 
 public interface Expression extends NodeGeneratable<InsnList>, Inferable {
 
+    public static final Expression UNINITIALIZED = new Expression() {
+
+        @Override
+        public Class<?> inferredType() {
+            return Object.class;
+        }
+
+        @Override
+        public InsnList toNode() {
+            InsnList insnList = new InsnList();
+            insnList.add(new InsnNode(Opcodes.NOP));
+            return insnList;
+        }
+    };
 }
