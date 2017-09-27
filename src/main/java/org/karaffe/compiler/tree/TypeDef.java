a4064e34e3e80dbf6b4bb52ee867e0609905b35b
nokok <nokok.kz@gmail.com>
2017-09-28 00:05:47 +0900

    コンストラクタを追加

--- src/main/java/org/karaffe/compiler/tree/TypeDef.java
+++ src/main/java/org/karaffe/compiler/tree/TypeDef.java
@@ -1,5 +1,9 @@
 package org.karaffe.compiler.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.karaffe.compiler.lexer.IdentifierToken;
 import org.karaffe.compiler.tree.base.AbstractNode;
 import org.karaffe.compiler.tree.base.Name;
 import org.karaffe.compiler.tree.base.NodeType;
@@ -8,13 +12,30 @@ public abstract class TypeDef extends AbstractNode {
 
     private final Name className;
 
-    public TypeDef(final Name className) {
-        super(NodeType.DEFCLASS, className.getPosition());
+    public TypeDef(final NodeType type, final Name className) {
+        super(type, className.getPosition());
         this.className = className;
     }
 
     public String getClassName() {
         return this.className.getText();
+    }
+
+    public static class ClassDef extends TypeDef {
+        private final Name superName;
+        private final List<VarDef> fields;
+        private final List<MethodDef> methodDefs;
+
+        public ClassDef(final Name className) {
+            this(className, null, null, null);
+        }
+
+        public ClassDef(final Name className, final Name superClassName, final List<VarDef> fieldDefs, final List<MethodDef> methodDefs) {
+            super(NodeType.DEFCLASS, className);
+            this.superName = superClassName == null ? new Name(new IdentifierToken.TypeName("Object")) : superClassName;
+            this.fields = fieldDefs == null ? new ArrayList<>(0) : fieldDefs;
+            this.methodDefs = methodDefs == null ? new ArrayList<>(0) : methodDefs;
+        }
     }
 
 }
