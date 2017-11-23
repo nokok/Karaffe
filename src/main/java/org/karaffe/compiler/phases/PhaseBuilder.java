48365f9d616a87b1c6a9e178d869a2176c0f3fa2
Noriyuki Kazusawa <nokok.kz@gmail.com>
2017-11-23 21:00:21 +0900

    PhaseBuilderの追加

---
+++ src/main/java/org/karaffe/compiler/phases/PhaseBuilder.java
@@ -0,0 +1,13 @@
+package org.karaffe.compiler.phases;
+
+import org.karaffe.compiler.CompilerContext;
+
+public class PhaseBuilder {
+
+    private final CompilerContext context;
+
+    public PhaseBuilder(CompilerContext context) {
+        this.context = context;
+    }
+
+}
