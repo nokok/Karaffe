c141288418c45b83f276333a13e55cc83db9e42e
Noriyuki Kazusawa <nokok.kz@gmail.com>
2017-12-03 21:34:02 +0900

    Range系クラスの追加

--- src/main/java/org/karaffe/compiler/pos/Pos.java
+++ src/main/java/org/karaffe/compiler/pos/Pos.java
@@ -11,12 +11,28 @@ public abstract class Pos {
         return new LineColumnPosition(line, column);
     }
 
+    public static Pos range(int startIndex, int endIndex) {
+        return new Range(startIndex, endIndex);
+    }
+
+    public static Pos rangeLocation(String filePath, int startIndex, int endIndex) {
+        return new RangeLocation(filePath, startIndex, endIndex);
+    }
+
+    public static Pos location(String filePath, int line, int column) {
+        return new Location(filePath, line, column);
+    }
+
     public Optional<NoPos> asNoPos() {
         return this.as(NoPos.class);
     }
 
     public Optional<LineColumnPosition> asLineAndColumn() {
         return this.as(LineColumnPosition.class);
+    }
+
+    public Optional<Location> asLocation() {
+        return this.as(Location.class);
     }
 
     private <T extends Pos> Optional<T> as(Class<T> clazz) {
@@ -39,12 +55,71 @@ class LineColumnPosition extends Pos {
     private final int column;
 
     public LineColumnPosition(int line, int column) {
+        if (line < 1) {
+            throw new IllegalArgumentException("line number < 1 :" + line);
+        }
+        if (column < 0) {
+            throw new IllegalArgumentException("column index is negative. :" + column);
+        }
         this.line = line;
         this.column = column;
+    }
+
+    public int line() {
+        return this.line;
+    }
+
+    public int column() {
+        return this.column;
     }
 
     @Override
     public String toString() {
         return this.line + ":" + this.column;
+    }
+}
+
+class Location extends Pos {
+    private final String filePath;
+    private final LineColumnPosition position;
+
+    public Location(String filePath, int line, int column) {
+        this.filePath = filePath;
+        this.position = new LineColumnPosition(line, column);
+    }
+
+    @Override
+    public String toString() {
+        return this.filePath + ":" + this.position.toString();
+    }
+}
+
+class Range extends Pos {
+    private int start;
+    private int end;
+
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return this.start + "~" + this.end;
+    }
+}
+
+class RangeLocation extends Pos {
+    private final String filePath;
+    private final Range range;
+
+    public RangeLocation(String filePath, int start, int end) {
+        this.filePath = filePath;
+        this.range = new Range(start, end);
+    }
+
+    @Override
+    public String toString() {
+        return this.filePath + ":" + this.range.toString();
     }
 }
