d441dcd0a426e340ddc19007f4cfe01c3bc9fc67
Noriyuki Kazusawa <nokok.kz@gmail.com>
2017-12-10 21:50:58 +0900

    staticファクトリメソッドとして別だし

--- src/main/java/org/karaffe/compiler/context/SourceStream.java
+++ src/main/java/org/karaffe/compiler/context/SourceStream.java
@@ -1,7 +1,11 @@
 package org.karaffe.compiler.context;
 
 import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.karaffe.compiler.util.SourceCodeContainer;
 
 public interface SourceStream {
 
@@ -9,4 +13,9 @@ public interface SourceStream {
 
     boolean hasSource();
 
+    public static SourceStream createSourceStream(String[] args) {
+        Set<File> sourceSet = Stream.of(args).filter(arg -> arg.endsWith(".krf")).map(File::new).collect(Collectors.toSet());
+        SourceCodeContainer container = new SourceCodeContainer(sourceSet);
+        return container;
+    }
 }
