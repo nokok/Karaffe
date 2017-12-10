3b78560e794555bae248a146f32ef1d37b333504
Noriyuki Kazusawa <nokok.kz@gmail.com>
2017-12-10 20:23:59 +0900

    YAGNI

--- src/main/java/org/karaffe/compiler/context/OutputSet.java
+++ src/main/java/org/karaffe/compiler/context/OutputSet.java
@@ -5,8 +5,6 @@ import org.karaffe.compiler.util.Report;
 import org.karaffe.compiler.util.Report.AdditionalInfo;
 
 public interface OutputSet {
-    public <T> void defNewClass(T def);
-
     public default void addErrorReport(String title, Position position, String message, AdditionalInfo... infomations) {
         this.addReport(Report.createError(title, position, message, infomations));
     }
