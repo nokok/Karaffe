a674bede880092943794f15f4e2297f05477a49d
Noriyuki Kazusawa <nokok.kz@gmail.com>
2017-12-27 23:51:51 +0900

    WIP

--- src/main/java/org/karaffe/Main.java
+++ src/main/java/org/karaffe/Main.java
@@ -1,8 +1,16 @@
 package org.karaffe;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
+import org.karaffe.compiler.CompilerFactory;
+import org.karaffe.compiler.KaraffeCompiler;
 import org.karaffe.compiler.util.DiagnosticInfo;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,25 +18,44 @@ public class Main {
     }
 
     private void run(String[] args) {
-
         System.out.println("Launching Karaffe Tools...");
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            System.out.println("\nKaraffeCompiler: UncaughtException: " + e.getMessage() + " , Thread: " + t.getName());
-            System.out.println("=====STACK TRACE=====");
+            System.err.println("\nKaraffeCompiler: UncaughtException: " + e.getMessage() + " , Thread: " + t.getName());
+            System.err.println("=====STACK TRACE=====");
             e.printStackTrace();
-            System.out.println("=====================");
-            System.out.println(DiagnosticInfo.INSTANCE.toString());
+            System.err.println("=====================");
+            System.err.println(DiagnosticInfo.INSTANCE.toString());
         });
 
+        Config config = ConfigFactory.parseResources("compiler.conf");
+        final List<String> terminateCommands;
+        if (config.hasPath("buildtool.terminateCommands")) {
+            terminateCommands = config.getStringList("buildtool.terminateCommands");
+        } else {
+            terminateCommands = Arrays.asList("exit", "quit");
+        }
+
+        Config scripts = config.getConfig("buildtool.scripts");
+
         try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("> ");
             while (scanner.hasNextLine()) {
-                String line = scanner.nextLine().trim();
-                if (line.equals("exit") || line.equals("quit")) {
+                String line = scanner.nextLine();
+                if (terminateCommands.contains(line)) {
                     break;
                 }
                 switch (line) {
                 case "compile":
                     System.out.println("Compiling...");
+                    KaraffeCompiler compiler = CompilerFactory.createCompiler(args);
+                    try {
+                        int v = compiler.call();
+                        if (v != 0) {
+                            // Show Warning
+                        }
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
                     System.out.println("Done.");
                     break;
                 case "clean":
@@ -37,7 +64,21 @@ public class Main {
                 case "run":
                     System.out.println("Loading MainClass...");
                     break;
+                case "\f":
+                    if (Platform.isWindows()) {
+                        try {
+                            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                        } catch (IOException | InterruptedException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    } else {
+                        System.out.println("");
+                    }
+                    break;
+                case "throw":
+                    throw new RuntimeException("Crash Requested");
                 }
+                System.out.print("> ");
             }
         } finally {
             System.out.println("Bye");
