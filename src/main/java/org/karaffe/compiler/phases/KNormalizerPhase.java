9e52118a833f18884b75aab9c166cc85bb8db361
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-01-22 00:37:40 +0900

    2回normalizeしていたことによるエラーを修正

--- src/main/java/org/karaffe/compiler/phases/KNormalizerPhase.java
+++ src/main/java/org/karaffe/compiler/phases/KNormalizerPhase.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.karaffe.compiler.context.CompilerConfig;
 import org.karaffe.compiler.phases.knormalize.KNormalizeVisitor;
 import org.karaffe.compiler.tree.CompileUnit;
+import org.karaffe.compiler.tree.base.Node;
 import org.karaffe.compiler.util.Traceable;
 
 public class KNormalizerPhase extends AbstractCompileUnitTransformer implements Traceable {
@@ -18,8 +19,8 @@ public class KNormalizerPhase extends AbstractCompileUnitTransformer implements 
     @Override
     public Optional<CompileUnit> transform(CompileUnit input) {
         KNormalizeVisitor visitor = new KNormalizeVisitor(input);
-        info(visitor.normalize().toString());
-        CompileUnit normalizedCompileUnit = (CompileUnit) visitor.normalize();
+        Node node = visitor.normalize();
+        CompileUnit normalizedCompileUnit = (CompileUnit) node;
         return Optional.ofNullable(normalizedCompileUnit);
     }
 
