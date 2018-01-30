35adae2c2eb8b09f57872b940b9b1578bf9083d9
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-01-31 00:10:53 +0900

    コンテキストがクエリを受け付けられるように修正

--- src/main/java/org/karaffe/compiler/context/TypeInferenceContext.java
+++ src/main/java/org/karaffe/compiler/context/TypeInferenceContext.java
@@ -1,5 +1,21 @@
 package org.karaffe.compiler.context;
 
 public class TypeInferenceContext {
+    private final String query;
 
+    private TypeInferenceContext() {
+        this("/");
+    }
+
+    private TypeInferenceContext(String query) {
+        this.query = query;
+    }
+
+    public static TypeInferenceContext create() {
+        return new TypeInferenceContext();
+    }
+
+    public static TypeInferenceContext withQuery(String query) {
+        return new TypeInferenceContext(query);
+    }
 }
