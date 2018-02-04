03a9c8bb68e0ace212e747ef95d16d8a9f7efab8
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-02-05 00:12:21 +0900

    インターフェースの更新

--- src/main/java/org/karaffe/compiler/types/InferResult.java
+++ src/main/java/org/karaffe/compiler/types/InferResult.java
@@ -1,37 +1,77 @@
 package org.karaffe.compiler.types;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.karaffe.compiler.context.TypeInferenceContext;
+import org.karaffe.compiler.tree.Name;
+
 import karaffe.core.Any;
 
-public class InferResult {
+public interface InferResult {
 
     public static String defaultTypeName() {
         return Any.class.getCanonicalName();
     }
 
+    public static InferResult failed() {
+        return new Failed();
+    }
+
     public static InferResult of(final Class<? extends Any> clazz) {
-        return new InferResult(clazz.getCanonicalName());
+        return new Successful(clazz.getCanonicalName());
+    }
+
+    public static InferResult of(final String... clazz) {
+        return new Successful(clazz);
+    }
+
+    public static InferResult of(final List<String> clazz) {
+        return new Successful(clazz);
     }
 
     public static InferResult ofEmpty() {
-        return new InferResult("karaffe.core.Any");
+        return new Successful("karaffe.core.Any");
     }
 
-    private final List<String> types;
-
-    public InferResult(final String... possibleTypes) {
-        this.types = new ArrayList<>(Arrays.asList(possibleTypes));
+    public static InferResult mayBeApplicable(InferResult ownerType, Name member) {
+        return new MayBeApplicable(ownerType, member);
     }
 
-    public Optional<String> getType() {
-        if (this.types.isEmpty()) {
-            return Optional.empty();
-        }
-        return Optional.of(this.types.get(0));
+    public static InferResult applying(InferResult applicable, List<InferResult> args) {
+        return new Applying(applicable, args);
     }
 
+    public static Undecidable noHint() {
+        return new NoHint();
+    }
+
+    public static Undecidable anyTarget(Name name) {
+        return new AnyTarget(name);
+    }
+
+    public default boolean isNotComplete() {
+        return !this.isComplete();
+    }
+
+    public boolean isComplete();
+
+    public boolean isFailed();
+
+    public ResultType getInferResultType();
+
+    public Optional<InferResult> compose(InferResult other, TypeInferenceContext context);
+
+    public default Optional<String> getType() {
+        return Optional.empty();
+    }
+
+    public enum ResultType {
+        FAILED,
+        SUCCESSFUL,
+        NO_HINT,
+        ANY_TARGET,
+        MAY_BE_APPLICABLE,
+        APPLYING
+    }
 }
