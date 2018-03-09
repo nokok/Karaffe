bd9c0c8d4c8064ec9bfefa22766c0c580de3f558
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-03-09 15:56:58 +0900

    メソッドの追加

--- src/main/java/karaffe/core/Console.java
+++ src/main/java/karaffe/core/Console.java
@@ -5,4 +5,8 @@ public class Console {
     public static void println(Object any) {
         System.out.println(any);
     }
+
+    public static void println() {
+        System.out.println();
+    }
 }
