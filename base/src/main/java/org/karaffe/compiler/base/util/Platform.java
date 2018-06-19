0492f4dd02f26fb88420fff0d28aeccac1c3e058
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-06-19 22:51:06 +0900

    NO_COLOR環境変数をサポート

--- base/src/main/java/org/karaffe/compiler/base/util/Platform.java
+++ base/src/main/java/org/karaffe/compiler/base/util/Platform.java
@@ -9,16 +9,43 @@ public enum Platform {
 
     INSTANCE,;
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BOLD = "\u001B[1m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    static {
+        String noColorVar = System.getenv("NO_COLOR");
+        if (noColorVar != null) {
+            ANSI_RESET = "";
+            ANSI_BOLD = "";
+            ANSI_BLACK = "";
+            ANSI_RED = "";
+            ANSI_GREEN = "";
+            ANSI_YELLOW = "";
+            ANSI_BLUE = "";
+            ANSI_PURPLE = "";
+            ANSI_CYAN = "";
+            ANSI_WHITE = "";
+        } else {
+            ANSI_RESET = "\u001B[0m";
+            ANSI_BOLD = "\u001B[1m";
+            ANSI_BLACK = "\u001B[30m";
+            ANSI_RED = "\u001B[31m";
+            ANSI_GREEN = "\u001B[32m";
+            ANSI_YELLOW = "\u001B[33m";
+            ANSI_BLUE = "\u001B[34m";
+            ANSI_PURPLE = "\u001B[35m";
+            ANSI_CYAN = "\u001B[36m";
+            ANSI_WHITE = "\u001B[37m";
+        }
+    }
+
+    public static final String ANSI_RESET;
+    public static final String ANSI_BOLD;
+    public static final String ANSI_BLACK;
+    public static final String ANSI_RED;
+    public static final String ANSI_GREEN;
+    public static final String ANSI_YELLOW;
+    public static final String ANSI_BLUE;
+    public static final String ANSI_PURPLE;
+    public static final String ANSI_CYAN;
+    public static final String ANSI_WHITE;
     private static PrintStream defaultStdOut = System.out;
     private static PrintStream stdOut = defaultStdOut;
     private static PrintStream defaultStdErr = System.err;
