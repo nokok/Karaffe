abb30f7e32b9a91c68b89e9da9d6c58a62ece388
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-07-13 00:18:24 +0900

    toString実装を追加

--- ir/src/main/java/org/karaffe/compiler/mir/util/InstructionList.java
+++ ir/src/main/java/org/karaffe/compiler/mir/util/InstructionList.java
@@ -4,6 +4,12 @@ import org.karaffe.compiler.mir.Instruction;
 import org.karaffe.compiler.mir.Instructions;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class InstructionList extends ArrayList<Instruction> implements Instructions {
+
+    @Override
+    public String toString() {
+        return String.join("\n", this.stream().map(i -> i + " " + i.getPosition()).collect(Collectors.toList()));
+    }
 }
