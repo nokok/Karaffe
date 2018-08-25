41385733baec0f6ee12fa89a83c8b0fe924578ee
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-08-26 00:34:07 +0900

    AutoReturnTaskの削除

--- frontend/src/main/java/org/karaffe/compiler/frontend/karaffe/tasks/postmir/PostMIRTask.java
+++ frontend/src/main/java/org/karaffe/compiler/frontend/karaffe/tasks/postmir/PostMIRTask.java
@@ -17,7 +17,6 @@ public class PostMIRTask extends AbstractTask implements NoDescriptionTask {
     private Set<Task> subTask = new LinkedHashSet<>();
 
     public PostMIRTask() {
-        this.subTask.add(new AutoReturnTask());
         this.subTask.add(new InsertDefaultConstructorTask());
     }
 
