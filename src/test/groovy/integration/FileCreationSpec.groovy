838a0f43fe7566a8f1edd30d009dc507a61052fa
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-11-10 20:31:46 +0900

    Add FileCreationSpec.groovy

---
+++ src/test/groovy/integration/FileCreationSpec.groovy
@@ -0,0 +1,24 @@
+package integration
+
+import org.karaffe.compiler.CompilerContext
+import org.karaffe.compiler.KaraffeCompiler
+import org.karaffe.compiler.util.KaraffeSource
+import spock.lang.Specification
+
+import java.nio.file.Paths
+
+class FileCreationSpec extends Specification {
+
+    def "Valid SimpleClass.class"() {
+        setup:
+        def context = new CompilerContext()
+        context.addSource(KaraffeSource.fromString("class SimpleClass"))
+        def compiler = new KaraffeCompiler(context)
+        compiler.run()
+
+        expect:
+        context.outputFiles.size() == 1
+        context.outputFiles.get(Paths.get("SimpleClass.class")) != null
+        context.hasNoOutputText()
+    }
+}
