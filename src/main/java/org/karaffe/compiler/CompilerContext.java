0d9d5b6c9352dfaaba01e495784ac62862dddf82
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-11-10 20:31:01 +0900

    Add some methods to CompilerContext

--- src/main/java/org/karaffe/compiler/CompilerContext.java
+++ src/main/java/org/karaffe/compiler/CompilerContext.java
@@ -1,12 +1,19 @@
 package org.karaffe.compiler;
 
+import org.karaffe.compiler.util.KaraffeSource;
+
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class CompilerContext {
-    private String[] rawArgs;
+    private String[] rawArgs = new String[0];
+    private List<KaraffeSource> sources = new ArrayList<>();
     private List<String> outputs = new ArrayList<>();
+    private Map<Path, byte[]> outputFiles = new HashMap<>();
 
     public String[] getRawArgs() {
         return rawArgs;
@@ -22,5 +29,29 @@ public class CompilerContext {
 
     public String getOutputText() {
         return String.join("\n", outputs);
+    }
+
+    public boolean hasNoOutputText() {
+        return this.outputs.isEmpty();
+    }
+
+    public void addSource(KaraffeSource source) {
+        this.sources.add(Objects.requireNonNull(source));
+    }
+
+    public List<KaraffeSource> getSources() {
+        return this.sources;
+    }
+
+    public void addOutputFile(Path target, byte[] content) {
+        this.outputFiles.put(Objects.requireNonNull(target), Objects.requireNonNull(content));
+    }
+
+    public Map<Path, byte[]> getOutputFiles() {
+        return outputFiles;
+    }
+
+    public boolean requireShowUsage() {
+        return this.rawArgs.length == 0 && this.getSources().isEmpty();
     }
 }
