f299181b6d294b204a858da321f2a3dce36d95ec
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-11-10 09:14:42 +0900

    Add usage

--- src/main/java/org/karaffe/compiler/Main.java
+++ src/main/java/org/karaffe/compiler/Main.java
@@ -2,6 +2,9 @@ package org.karaffe.compiler;
 
 public class Main {
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            System.out.println("Usage:");
+            System.out.println("  krfc <options> <sources>");
+        }
     }
 }
