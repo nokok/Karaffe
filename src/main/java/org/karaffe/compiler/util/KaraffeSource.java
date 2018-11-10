40598743966755ffeed253b6f9b4ca972ba92616
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-11-10 20:30:10 +0900

    Add KaraffeSource::fromPath

--- src/main/java/org/karaffe/compiler/util/KaraffeSource.java
+++ src/main/java/org/karaffe/compiler/util/KaraffeSource.java
@@ -1,5 +1,9 @@
 package org.karaffe.compiler.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
 
 public class KaraffeSource implements CharSequence {
@@ -10,8 +14,18 @@ public class KaraffeSource implements CharSequence {
         this.source = Objects.requireNonNull(source);
     }
 
+    private KaraffeSource(Path path) throws IOException {
+        Objects.requireNonNull(path);
+        List<String> strings = Files.readAllLines(path);
+        this.source = strings.stream().reduce((l, r) -> l + "\n" + r).orElse("");
+    }
+
     public static KaraffeSource fromString(String source) {
         return new KaraffeSource(source);
+    }
+
+    public static KaraffeSource fromPath(Path path) throws IOException {
+        return new KaraffeSource(path);
     }
 
     @Override
