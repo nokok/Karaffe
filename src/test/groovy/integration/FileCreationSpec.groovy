c10e82b0a4cdbfd3283c015358593d17e134ad48
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-11-14 21:21:53 +0900

    Add Test

--- src/test/groovy/integration/FileCreationSpec.groovy
+++ src/test/groovy/integration/FileCreationSpec.groovy
@@ -5,6 +5,7 @@ import org.karaffe.compiler.KaraffeCompiler
 import org.karaffe.compiler.util.KaraffeSource
 import spock.lang.Specification
 
+import java.nio.file.Files
 import java.nio.file.Paths
 
 class FileCreationSpec extends Specification {
@@ -21,11 +22,13 @@ class FileCreationSpec extends Specification {
         context.outputFiles.size() == 1
         context.outputFiles.get(Paths.get("SimpleClass.class")) != null
         context.hasNoOutputText()
+        !Files.exists(Paths.get("SimpleClass.class"))
     }
 
     def "Invalid Class 1A"() {
         setup:
         def context = new CompilerContext()
+        context.setRawArgs(["--dry-run"] as String[])
         context.addSource(KaraffeSource.fromString("class 1A"))
         def compiler = new KaraffeCompiler(context)
         compiler.run()
@@ -34,5 +37,7 @@ class FileCreationSpec extends Specification {
         context.outputFiles.size() == 0
         context.outputFiles.get(Paths.get("A.class")) == null
         !context.hasNoOutputText()
+        !Files.exists(Paths.get("A.class"))
+        !Files.exists(Paths.get("1A.class"))
     }
 }
