ab8519c9ca87eb3f6bdb087e492a55919a8ff608
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-11-17 00:11:09 +0900

    Add WarningSpec

---
+++ src/test/groovy/integration/WarningSpec.groovy
@@ -0,0 +1,25 @@
+package integration
+
+import org.karaffe.compiler.CompilerContext
+import org.karaffe.compiler.KaraffeCompiler
+import org.karaffe.compiler.util.KaraffeSource
+import spock.lang.Specification
+
+class WarningSpec extends Specification {
+    def "naming"() {
+        setup:
+        def context = new CompilerContext()
+        context.setRawArgs(["--dry-run"] as String[])
+        context.addSource(KaraffeSource.fromString(source))
+        def compiler = new KaraffeCompiler(context)
+        compiler.run()
+
+        expect:
+        context.outputFiles.size() == size
+        context.outputText == outputText
+
+        where:
+        source              || size || outputText
+        "class simpleClass" || 1    || "[warning] class name must be PascalCase(u)"
+    }
+}
