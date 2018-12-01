8a302679ad6b2b872086ce0f1d3e7419a892370e
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-12-01 22:12:08 +0900

    Use Console.print

--- src/main/java/org/karaffe/compiler/Main.java
+++ src/main/java/org/karaffe/compiler/Main.java
@@ -1,11 +1,13 @@
 package org.karaffe.compiler;
 
+import karaffe.core.Console;
+
 public class Main {
     public static void main(String[] args) {
         CompilerContext context = new CompilerContext();
         context.parseRawArgs(args);
         KaraffeCompiler compiler = new KaraffeCompiler(context);
         compiler.run();
-        System.out.print(context.hasNoOutputText() ? "" : context.getOutputText() + "\n");
+        Console.print(context.hasOutputText() ? context.getOutputText() + "\n" : "");
     }
 }
