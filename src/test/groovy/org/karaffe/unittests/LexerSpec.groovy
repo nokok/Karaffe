5ec7813468bf353b47fd4265c5226f6da9ef389e
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-12-01 23:21:57 +0900

    Add tests

--- src/test/groovy/org/karaffe/unittests/LexerSpec.groovy
+++ src/test/groovy/org/karaffe/unittests/LexerSpec.groovy
@@ -24,15 +24,21 @@ class LexerSpec extends Specification {
         token.getType() == tokenType
 
         where:
-        source    || tokenType
-        "A"       || KaraffeLexer.Identifier
-        "Hello"   || KaraffeLexer.Identifier
-        "a"       || KaraffeLexer.Identifier
-        "z"       || KaraffeLexer.Identifier
-        "HB"      || KaraffeLexer.Identifier
-        "H1"      || KaraffeLexer.Identifier
-        "1"       || KaraffeLexer.IntegerLiteral
-        '"Hello"' || KaraffeLexer.StringLiteral
+        source       || tokenType
+        "A"          || KaraffeLexer.Identifier
+        "Hello"      || KaraffeLexer.Identifier
+        "a"          || KaraffeLexer.Identifier
+        "z"          || KaraffeLexer.Identifier
+        "HB"         || KaraffeLexer.Identifier
+        "H1"         || KaraffeLexer.Identifier
+        "1"          || KaraffeLexer.IntegerLiteral
+        '"Hello"'    || KaraffeLexer.StringLiteral
+        "{"          || KaraffeLexer.LBRACE
+        "}"          || KaraffeLexer.RBRACE
+        "+"          || KaraffeLexer.PLUS
+        "entrypoint" || KaraffeLexer.ENTRYPOINT
+        "class"      || KaraffeLexer.CLASS
+        "CLASS"      || KaraffeLexer.Identifier
     }
 
     @Unroll
@@ -54,8 +60,32 @@ class LexerSpec extends Specification {
         errorMessage == expectedErrorMessage
 
         where:
-        source || expectedErrorMessage
-        "_"    || "Syntax Error at 1 : 0 , token recognition error at: '_'"
+        source      || expectedErrorMessage
+        "_"         || "Syntax Error at 1 : 0 , token recognition error at: '_'"
+        "HOGE_FUGA" || "Syntax Error at 1 : 4 , token recognition error at: '_'"
+        "100,000"   || "Syntax Error at 1 : 3 , token recognition error at: ','"
     }
 
+    @Unroll
+    def "test StringLiteral #source"() {
+        setup:
+        def lexer = new KaraffeLexer(CharStreams.fromString(source))
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(SyntaxSpec.DEFULT_ERROR_LISTENER)
+        def stream = new CommonTokenStream(lexer)
+        stream.consume()
+        def token = stream.get(0)
+        def tokenText = token.getText()
+
+        expect:
+        tokenText == source
+        token.getType() == KaraffeLexer.StringLiteral
+
+        where:
+        source << [
+                '""',
+                '"1234"',
+                '"Hello"'
+        ]
+    }
 }
