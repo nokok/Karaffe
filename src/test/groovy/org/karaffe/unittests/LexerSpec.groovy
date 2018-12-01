cfc44ca8264c3134163ca73ec63ee4d41fa42d2a
Noriyuki Kazusawa <nokok.kz@gmail.com>
2018-12-01 22:01:59 +0900

    Add LexerSpec

---
+++ src/test/groovy/org/karaffe/unittests/LexerSpec.groovy
@@ -0,0 +1,61 @@
+package org.karaffe.unittests
+
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.karaffe.compiler.frontend.karaffe.antlr.KaraffeLexer
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class LexerSpec extends Specification {
+
+    @Unroll
+    def "token Type #source"() {
+        setup:
+        def lexer = new KaraffeLexer(CharStreams.fromString(source))
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(SyntaxSpec.DEFULT_ERROR_LISTENER)
+        def stream = new CommonTokenStream(lexer)
+        stream.consume()
+        def token = stream.get(0)
+        def tokenText = token.getText()
+
+        expect:
+        tokenText == source
+        token.getType() == tokenType
+
+        where:
+        source    || tokenType
+        "A"       || KaraffeLexer.Identifier
+        "Hello"   || KaraffeLexer.Identifier
+        "a"       || KaraffeLexer.Identifier
+        "z"       || KaraffeLexer.Identifier
+        "HB"      || KaraffeLexer.Identifier
+        "H1"      || KaraffeLexer.Identifier
+        "1"       || KaraffeLexer.IntegerLiteral
+        '"Hello"' || KaraffeLexer.StringLiteral
+    }
+
+    @Unroll
+    def "invalid token Type #source"() {
+        setup:
+        def lexer = new KaraffeLexer(CharStreams.fromString(source))
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(SyntaxSpec.DEFULT_ERROR_LISTENER)
+        def stream = new CommonTokenStream(lexer)
+        String errorMessage = ""
+        try {
+            stream.consume()
+            assert false: "assertion failed ${source}"
+        } catch (RuntimeException e) {
+            errorMessage = e.getMessage()
+        }
+
+        expect:
+        errorMessage == expectedErrorMessage
+
+        where:
+        source || expectedErrorMessage
+        "_"    || "Syntax Error at 1 : 0 , token recognition error at: '_'"
+    }
+
+}
