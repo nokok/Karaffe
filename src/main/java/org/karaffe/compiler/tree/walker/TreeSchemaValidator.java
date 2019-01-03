eeb938edee0dce3f0438b88003aa2fefbffd9328
Noriyuki Kazusawa <nokok.kz@gmail.com>
2019-01-03 15:15:55 +0900

    Update validator

--- src/main/java/org/karaffe/compiler/tree/walker/TreeSchemaValidator.java
+++ src/main/java/org/karaffe/compiler/tree/walker/TreeSchemaValidator.java
@@ -8,6 +8,9 @@ import static org.karaffe.compiler.tree.NodeType.DefClass;
 import static org.karaffe.compiler.tree.NodeType.Identifier;
 import static org.karaffe.compiler.tree.NodeType.Modifier;
 import static org.karaffe.compiler.tree.NodeType.Modifiers;
+import static org.karaffe.compiler.tree.NodeType.Parameter;
+import static org.karaffe.compiler.tree.NodeType.Parameters;
+import static org.karaffe.compiler.tree.NodeType.ReturnType;
 import static org.karaffe.compiler.tree.NodeType.Select;
 import static org.karaffe.compiler.tree.NodeType.SourceFile;
 import static org.karaffe.compiler.tree.NodeType.SuperClass;
@@ -26,11 +29,27 @@ public class TreeSchemaValidator extends TreeWalkerAdapter {
     @Override
     void onTypeName(Tree tree) {
         assert !tree.getName().isEmpty();
+        assert !tree.hasChildren();
+    }
+
+    @Override
+    void onModifier(Tree tree) {
+        String name = tree.getName();
+        assert !name.isEmpty();
+        assert name.equals("public") || name.equals("static");
+        assert !tree.hasChildren();
+    }
+
+    @Override
+    void onThis(Tree tree) {
+        assert tree.getName().isEmpty();
+        assert !tree.hasChildren();
     }
 
     @Override
     void onIdentifier(Tree tree) {
         assert !tree.getName().isEmpty();
+        assert !tree.hasChildren();
     }
 
     @Override
@@ -42,25 +61,41 @@ public class TreeSchemaValidator extends TreeWalkerAdapter {
     }
 
     @Override
+    void onStringLiteral(Tree tree) {
+        assert !tree.hasChildren();
+    }
+
+    @Override
     void onReturnType(Tree tree) {
         assert tree.indexOf(TypeName) == 0;
+        assert tree.getChildren().size() == 1;
     }
 
     @Override
     void onSuperClass(Tree tree) {
         assert tree.indexOf(TypeName) == 0;
+        assert tree.getChildren().size() == 1;
     }
 
     @Override
     void onParameter(Tree tree) {
         assert !tree.getName().isEmpty();
         assert tree.indexOf(TypeName) == 0;
+        assert tree.getChildren().size() == 1;
+    }
+
+    @Override
+    void onParameters(Tree tree) {
+        for (Tree parameter : tree.getChildren()) {
+            assert parameter.getNodeType() == Parameter;
+        }
     }
 
     @Override
     void onApply(Tree tree) {
         assert tree.indexOf(Select) == 0;
         assert tree.indexOf(Arguments) == 1;
+        assert tree.getChildren().size() == 2;
     }
 
     @Override
@@ -70,14 +105,54 @@ public class TreeSchemaValidator extends TreeWalkerAdapter {
     }
 
     @Override
+    void onIntLiteral(Tree tree) {
+        assert !tree.hasChildren();
+    }
+
+    @Override
+    void onErrorTree(Tree tree) {
+
+    }
+
+    @Override
+    void onDefVar(Tree tree) {
+        assert !tree.getName().isEmpty();
+    }
+
+    @Override
+    void onDefMethod(Tree tree) {
+        assert tree.indexOf(Modifiers) == 0;
+        assert tree.indexOf(ReturnType) == 1;
+        assert tree.indexOf(Parameters) == 2;
+        assert tree.indexOf(Body) == 3;
+        assert tree.getChildren().size() == 4;
+    }
+
+    @Override
     void onDefClass(Tree tree) {
         assert tree.indexOf(SuperClass) == 0;
         assert tree.indexOf(Modifiers) == 1;
         assert tree.indexOf(Body) == 2;
+        assert tree.getChildren().size() == 3;
     }
 
     @Override
     void onModifiers(Tree tree) {
         assert tree.indexOf(Modifier) == 0;
+    }
+
+    @Override
+    void onArgument(Tree tree) {
+        assert tree.getChildren().size() == 1;
+    }
+
+    @Override
+    void onArguments(Tree tree) {
+        assert tree.getName().isEmpty();
+    }
+
+    @Override
+    void onBody(Tree tree) {
+        assert tree.getName().isEmpty();
     }
 }
