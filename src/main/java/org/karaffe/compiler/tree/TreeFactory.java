f72b91602abc0918c86177afa28d893583093e45
Noriyuki Kazusawa <nokok.kz@gmail.com>
2019-02-04 00:07:41 +0900

    Add factory method

--- src/main/java/org/karaffe/compiler/tree/TreeFactory.java
+++ src/main/java/org/karaffe/compiler/tree/TreeFactory.java
@@ -2,6 +2,8 @@ package org.karaffe.compiler.tree;
 
 import org.karaffe.compiler.util.Position;
 
+import java.util.Arrays;
+
 public final class TreeFactory {
   public static Tree newTree(NodeType nodeType) {
     return new SimpleTree(nodeType);
@@ -17,5 +19,29 @@ public final class TreeFactory {
 
   public static Tree newTree(NodeType nodeType, String name, Position position) {
     return new NamedPositionTree(nodeType, name, position);
+  }
+
+  public static Tree newTree(NodeType nodeType, Tree... children) {
+    Tree tree = TreeFactory.newTree(nodeType);
+    tree.addAllChildren(Arrays.asList(children));
+    return tree;
+  }
+
+  public static Tree newTree(NodeType nodeType, String name, Tree... children) {
+    Tree tree = TreeFactory.newTree(nodeType, name);
+    tree.addAllChildren(Arrays.asList(children));
+    return tree;
+  }
+
+  public static Tree newTree(NodeType nodeType, Position position, Tree... children) {
+    Tree tree = TreeFactory.newTree(nodeType, position);
+    tree.addAllChildren(Arrays.asList(children));
+    return tree;
+  }
+
+  public static Tree newTree(NodeType nodeType, String name, Position position, Tree... children) {
+    Tree tree = TreeFactory.newTree(nodeType, name, position);
+    tree.addAllChildren(Arrays.asList(children));
+    return tree;
   }
 }
