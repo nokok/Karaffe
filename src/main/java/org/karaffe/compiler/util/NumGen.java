4230ba39836fd49cdbfca645a8a916aa8f8c13f5
Noriyuki Kazusawa <nokok.kz@gmail.com>
2019-03-21 00:10:11 +0900

    Update NumGen

--- src/main/java/org/karaffe/compiler/util/NumGen.java
+++ src/main/java/org/karaffe/compiler/util/NumGen.java
@@ -1,9 +1,9 @@
 package org.karaffe.compiler.util;
 
 public final class NumGen {
-  private static long value = 0;
+  private long value = 0;
 
-  public static long next() {
+  public long next() {
     return value++;
   }
 }
