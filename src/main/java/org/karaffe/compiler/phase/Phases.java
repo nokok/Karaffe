0dda8c2d21efc71d570ca8bc7b6f199b369a15bd
Noriyuki Kazusawa <nokok.kz@gmail.com>
2019-07-21 23:59:05 +0900

    Impl Phases

--- src/main/java/org/karaffe/compiler/phase/Phases.java
+++ src/main/java/org/karaffe/compiler/phase/Phases.java
@@ -1,17 +1,28 @@
 package org.karaffe.compiler.phase;
 
+import org.karaffe.compiler.phase.util.ShowVersionPhase;
 import org.karaffe.compiler.util.CompilerContext;
 import org.karaffe.compiler.util.args.Flag;
+
+import java.util.Optional;
 
 public interface Phases extends Iterable<Phase> {
   static Phases createPhasesFromContext(CompilerContext context) {
     MutablePhases phases = new SequentialPhases();
     if (context.hasFlag(Flag.VERSION)) {
-
+      phases.add(new ShowVersionPhase());
     }
     return phases;
   }
 
   void executeAll(CompilerContext context);
 
+  default Optional<Phase> getPhase(String phaseName) {
+    for (Phase phase : this) {
+      if (phase.getName().equals(phaseName)) {
+        return Optional.of(phase);
+      }
+    }
+    return Optional.empty();
+  }
 }
---
+++ src/test/groovy/org/karaffe/integration/PhasesSpec.groovy
@@ -0,0 +1,18 @@
+package org.karaffe.integration
+
+import org.karaffe.compiler.phase.Phases
+import org.karaffe.compiler.util.CompilerContext
+import org.karaffe.compiler.util.StartupEnv
+import spock.lang.Specification
+
+class PhasesSpec extends Specification {
+
+  def "version"() {
+    setup:
+    def ctx = CompilerContext.createInitialContext(StartupEnv.create(["--version"] as String[], [:]))
+    def phases = Phases.createPhasesFromContext(ctx)
+
+    expect:
+    phases.getPhase("show-version").isPresent()
+  }
+}
