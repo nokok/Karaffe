72f06b8e9953ed8e9c6c43552a97dc3a357b2ac1
Noriyuki Kazusawa <nokok.kz@gmail.com>
2019-07-27 19:06:30 +0900

    Update test

--- src/test/groovy/org/karaffe/unittests/ByteCodeSelectorSpec.groovy
+++ src/test/groovy/org/karaffe/unittests/ByteCodeSelectorSpec.groovy
@@ -13,49 +13,22 @@ class ByteCodeSelectorSpec extends Specification {
     BytecodeSelectorForNumber.fromInt(value).opcode == opcodes
 
     where:
-    value || opcodes
-    -1    || Opcodes.ICONST_M1
-    0     || Opcodes.ICONST_0
-    1     || Opcodes.ICONST_1
-    2     || Opcodes.ICONST_2
-    3     || Opcodes.ICONST_3
-    4     || Opcodes.ICONST_4
-    5     || Opcodes.ICONST_5
-  }
-
-  @Unroll
-  def "byte #value"() {
-    expect:
-    BytecodeSelectorForNumber.fromInt(value).opcode == opcodes
-
-    where:
-    value || opcodes
-    6     || Opcodes.BIPUSH
-    -2    || Opcodes.BIPUSH
-    -127  || Opcodes.BIPUSH
-    127   || Opcodes.BIPUSH
-  }
-
-  @Unroll
-  def "short #value"() {
-    expect:
-    BytecodeSelectorForNumber.fromInt(value).opcode == opcodes
-
-    where:
     value  || opcodes
+    -1     || Opcodes.ICONST_M1
+    0      || Opcodes.ICONST_0
+    1      || Opcodes.ICONST_1
+    2      || Opcodes.ICONST_2
+    3      || Opcodes.ICONST_3
+    4      || Opcodes.ICONST_4
+    5      || Opcodes.ICONST_5
+    6      || Opcodes.BIPUSH
+    -2     || Opcodes.BIPUSH
+    -127   || Opcodes.BIPUSH
+    127    || Opcodes.BIPUSH
     128    || Opcodes.SIPUSH
     129    || Opcodes.SIPUSH
     32767  || Opcodes.SIPUSH
     -32768 || Opcodes.SIPUSH
-  }
-
-  @Unroll
-  def "int #value"() {
-    expect:
-    BytecodeSelectorForNumber.fromInt(value).opcode == opcodes
-
-    where:
-    value  || opcodes
     -32769 || Opcodes.LDC
     32768  || Opcodes.LDC
   }
