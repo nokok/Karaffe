79c2bd77f59d6154cdf55dde4fddf73521cb4583
Noriyuki Kazusawa <nokok.kz@gmail.com>
2019-08-28 14:43:34 +0900

    Change report format

--- src/main/java/org/karaffe/compiler/phase/util/ShowReportsPhase.java
+++ src/main/java/org/karaffe/compiler/phase/util/ShowReportsPhase.java
@@ -3,6 +3,7 @@ package org.karaffe.compiler.phase.util;
 import org.karaffe.compiler.phase.Phase;
 import org.karaffe.compiler.util.CompilerContext;
 import org.karaffe.compiler.util.report.Report;
+import org.karaffe.compiler.util.report.ReportType;
 
 public class ShowReportsPhase implements Phase {
   @Override
@@ -13,11 +14,21 @@ public class ShowReportsPhase implements Phase {
   @Override
   public void execute(CompilerContext context) {
     for (Report report : context.getReports()) {
-      if (report.hasPosition()) {
-        context.writeLine(report.getHeader() + " at " + report.getPosition());
+      String h;
+      if (report.getReportType() == ReportType.ERROR) {
+        h = "ERROR | ";
+      } else if (report.getReportType() == ReportType.WARN) {
+        h = "WARN  | ";
       } else {
-        context.writeLine(report.getHeader());
+        h = "";
       }
+      String title;
+      if (report.hasPosition()) {
+        title = report.getHeader() + " at " + report.getPosition();
+      } else {
+        title = report.getHeader();
+      }
+      context.writeLine(h + title);
       report.getBody().ifPresent(context::writeLine);
     }
   }
